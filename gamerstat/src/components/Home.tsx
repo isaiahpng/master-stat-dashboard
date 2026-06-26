d8d683b921148fdd21cb31c08b81aa2311c0d74b
isaiahpng <100270360+isaiahpng@users.noreply.github.com>
2026-06-26 08:57:37 -0500

    Lots of Updates
    Implemented tailwind css bento boxes. Cleaned up styling and optimized .css files.

--- gamerstat/src/components/Home.tsx
+++ gamerstat/src/components/Home.tsx
@@ -1,13 +1,14 @@
 import bgImage from "../assets/bg.jpg";
+import BentoSection from "./BentoSection";
 import "./Home.css";
 
 function Home() {
     return (
         <main className="home">
             <img src={bgImage} className="home-bg" alt="" />
-            <img src={bgImage} className="home-bg-glow" alt="" />
 
-            <section className="hero-content"></section>
+            <div className="home-overlay" />
+            <BentoSection />
         </main>
     );
 }
